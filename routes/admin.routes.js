063506615dd8e9119b3a37d65a3ba869f1253d2b
Poom <poompongphun@gmail.com>
2021-08-17 18:36:23 +0700

    optimize code

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -3,26 +3,19 @@ const router = express.Router();
 const passport = require("../Middleware/auth/passport.middleware");
 const userControllers = require("../Controllers/admin/users.controllers");
 const playersControllers = require("../Controllers/admin/players.controller");
+const authMiddleware = passport.authenticate("jwt", { session: false });
 
 /* create user */
-router.post(
-  "/users/create",
-  passport.authenticate("jwt", { session: false }),
-  userControllers.createUser
-);
+router.post("/users/create", authMiddleware, userControllers.createUser);
 
 /* create player */
 router.post(
   "/players/create",
-  passport.authenticate("jwt", { session: false }),
+  authMiddleware,
   playersControllers.createPlayers
 );
 
 /* add player coins */
-router.post(
-  "/players/:id/coin",
-  passport.authenticate("jwt", { session: false }),
-  playersControllers.addCoins
-);
+router.post("/players/:id/coin", authMiddleware, playersControllers.addCoins);
 
 module.exports = router;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,15 +2,12 @@ const express = require("express");
 const router = express.Router();
 const passport = require("../Middleware/auth/passport.middleware");
 const authControllers = require("../Controllers/auth.controllers");
+const authMiddleware = passport.authenticate("jwt", { session: false });
 
 /* local login */
 router.post("/login", authControllers.authLogin);
 
-// me data
-router.get(
-  "/me",
-  passport.authenticate("jwt", { session: false }),
-  authControllers.authMe
-);
+/* get me data */
+router.get("/me", authMiddleware, authControllers.authMe);
 
 module.exports = router;
