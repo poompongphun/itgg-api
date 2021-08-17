ccf764c6e9bf3d74e5a39f72a6ab136ef42a0428
Poom <poompongphun@gmail.com>
2021-08-17 18:36:04 +0700

    add find by gate

--- Controllers/players.controllers.js
+++ Controllers/players.controllers.js
@@ -2,15 +2,17 @@ const player = require("../Models/players");
 
 /* GET player data. */
 const getPlayers = async (req, res, next) => {
-  const searchQuery = req.query.search || "";
+  const searchQuery = new RegExp(RegExp(req.query.search || ""), "i");
+  const gateQuery = new RegExp(RegExp(req.query.gate || ""), "i");
   const limit = parseInt(escape(req.query.limit)) || 10;
   const players = await player
     .find({
       $or: [
-        { std_id: { $regex: new RegExp(RegExp(searchQuery), "i") } },
-        { name: { $regex: new RegExp(RegExp(searchQuery), "i") } },
-        { nickname: { $regex: new RegExp(RegExp(searchQuery), "i") } },
+        { std_id: { $regex: searchQuery } },
+        { name: { $regex: searchQuery } },
+        { nickname: { $regex: searchQuery } },
       ],
+      house: { $regex: gateQuery },
     })
     .select({ _id: false, __v: false })
     .limit(limit)
