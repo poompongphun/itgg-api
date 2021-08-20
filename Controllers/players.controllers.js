2f34a4c58ae822f7bd501b97a3dabc9ad06dfbb4
Poom <poompongphun@gmail.com>
2021-08-20 22:54:31 +0700

    fix query gate

--- Controllers/players.controllers.js
+++ Controllers/players.controllers.js
@@ -3,17 +3,25 @@ const player = require("../Models/players");
 /* GET player data. */
 const getPlayers = async (req, res, next) => {
   const searchQuery = new RegExp(RegExp(req.query.search || ""), "i");
-  const gateQuery = new RegExp(RegExp(req.query.gate || ""), "i");
+  const gateQuery = req.query.gate || "";
   const limit = parseInt(escape(req.query.limit)) || 10;
+  const or = [
+    { std_id: { $regex: searchQuery } },
+    { name: { $regex: searchQuery } },
+    { discord_id: { $regex: searchQuery } },
+    { nickname: { $regex: searchQuery } },
+  ];
+  const query =
+    req.query.gate !== undefined
+      ? {
+          $or: or,
+          house: gateQuery,
+        }
+      : {
+          $or: or,
+        };
   const players = await player
-    .find({
-      $or: [
-        { std_id: { $regex: searchQuery } },
-        { name: { $regex: searchQuery } },
-        { nickname: { $regex: searchQuery } },
-      ],
-      house: { $regex: gateQuery },
-    })
+    .find(query)
     .select({ _id: false, __v: false })
     .limit(limit)
     .sort({ coin: -1 });
